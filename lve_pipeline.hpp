c01f763bb4bba1942a61a1c301ee656662423507
Ben Mwanzia <youngmwanzia@gmail.com>
2021-06-15 08:13:10 +0300

    add renderpass and pipelineLayout to pipelineconfig

--- lve_pipeline.hpp
+++ lve_pipeline.hpp
@@ -11,7 +11,6 @@ namespace m_lve {
     struct PipelineConfigInfo {
         VkViewport viewport;
         VkRect2D scissor;
-        VkPipelineViewportStateCreateInfo viewportInfo;
         VkPipelineInputAssemblyStateCreateInfo inputAssemblyInfo;
         VkPipelineRasterizationStateCreateInfo rasterizationInfo;
         VkPipelineMultisampleStateCreateInfo multisampleInfo;
