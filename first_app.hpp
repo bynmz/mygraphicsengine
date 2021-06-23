2f0425b636e302513613c9e0d7c4c8dd0e675449
Ben Mwanzia <youngmwanzia@gmail.com>
2021-06-23 08:23:00 +0300

    2d transformations

--- first_app.hpp
+++ first_app.hpp
@@ -2,9 +2,9 @@
 
 #include "lve_window.hpp"
 #include "lve_pipeline.hpp"
+#include "lve_game_object.hpp"
 #include "lve_device.hpp"
 #include "lve_swap_chain.hpp"
-#include "lve_model.hpp"
 
 // std
 #include <memory>
@@ -15,7 +15,7 @@ namespace m_lve
     class FirstApp
     {
     private:
-        void loadModels(); 
+        void loadGameObjects(); 
         void createPipelineLayout();
         void createPipeline();
         void createCommandBuffers();
@@ -23,6 +23,7 @@ namespace m_lve
         void drawFrame();
         void recreateSwapChain();
         void recordCommandBuffer(int imageIndex);
+        void renderGameObjects(VkCommandBuffer commandBuffer);
 
         LveWindow lveWindow{WIDTH, HEIGHT, "Hello Vulkan!"};
         LveDevice lveDevice{lveWindow};
@@ -30,7 +31,7 @@ namespace m_lve
         std::unique_ptr<LvePipeline> lvePipeline;
         VkPipelineLayout pipelineLayout;
         std::vector<VkCommandBuffer> commandBuffers;
-        std::unique_ptr<LveModel> lveModel;
+        std::vector<LveGameObject> gameObjects;
 
     public:
         static constexpr int WIDTH = 800;
