d92f0d14d48904fab7e9b69af60324c754127dbf
Ben Mwanzia <youngmwanzia@gmail.com>
2021-06-30 07:42:12 +0300

    3D Transforms

--- lve_model.hpp
+++ lve_model.hpp
@@ -12,7 +12,7 @@ namespace m_lve {
         public:
             struct Vertex
             {
-                glm::vec2 position;
+                glm::vec3 position;
                 glm::vec3 color;
 
                 static std::vector<VkVertexInputBindingDescription> getBindingDescriptions();
